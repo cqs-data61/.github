258309fcf2557fd4397b2a84ebcebc2aaab43dec
Jang, Seung (Data61, Marsfield) <Seung.Jang@data61.csiro.au>
2024-10-22 16:20:01 +1100

    Reverted

--- .github/scripts/aggregate.py
+++ .github/scripts/aggregate.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 # GitHub username (can be set via environment variable in secrets)
-GITHUB_USER = os.getenv('USER_NAME')
+USER_NAME = os.getenv('USER_NAME')
 TOKEN = os.getenv('PAT_TOKEN')  # Get from GitHub secrets
 
 # Headers for authentication
@@ -16,26 +16,15 @@ headers = {
 def get_repositories(user):
     repos = []
     page = 1
-    '''
     while True:
         url = f"https://api.github.com/users/{user}/repos?page={page}&per_page=100"
-        print(url)
         response = requests.get(url, headers=headers)
         data = response.json()
-        print(data)
         if len(data) == 0:
             break
         repos.extend(data)
         page += 1
-    '''
-
-    url = f"https://api.github.com/users/{user}/repos?page={page}&per_page=100"
-    print(url)
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    print(data)
-
-    return data
+    return repos
 
 
 def get_repo(repo):
@@ -103,4 +92,4 @@ def aggregate_github_stats(user):
 
 # Run the script
 if __name__ == "__main__":
-    aggregate_github_stats(GITHUB_USER)
+    aggregate_github_stats(USER_NAME)
